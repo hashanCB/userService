dab6219fc6462c6e16366f7145f9f3b4528badb8
chanaka <hashan.chanaka96@gmail.com>
2025-02-13 06:02:26 +0000

    aws deplay part

--- script.groovy
+++ script.groovy
@@ -1,8 +1,8 @@
   def Awsdeplay(String ImageName){
 
-        script.sshagent(['aws-ec2-userser']) {
-            script.sh 'scp -o StrictHostKeyChecking=no docker-compose.yml ec2-user@13.60.196.175:/home/ec2-user'
-            script.sh """
+        sshagent(['aws-ec2-userser']) {
+            sh 'scp -o StrictHostKeyChecking=no docker-compose.yml ec2-user@13.60.196.175:/home/ec2-user'
+            sh """
             ssh -o StrictHostKeyChecking=no ec2-user@13.60.196.175 '
                 export IMAGE_NAME=${ImageName}
                 docker-compose -f /home/ec2-user/docker-compose.yml up --detach
